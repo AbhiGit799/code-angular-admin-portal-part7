5dca0e1919a4ee08e535f30444638628645f75a5
Abhishek Choubey <choubeyabhishek53@gmail.com>
2023-07-30 17:38:40 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { LayoutComponent } from './shared/components/layout/layout.component';
+import { contentRoutes } from './shared/routes/content.routes';
+
+const routes: Routes = [
+
+  // { path: 'auth/login', component: LoginComponent },
+  
+    // AuthGuard
+    { path: '', component: LayoutComponent, children: contentRoutes }
+  
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
